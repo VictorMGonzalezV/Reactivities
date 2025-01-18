ffd64eab0119eddc3e8ad437091ff5dce617aefd
VictorMGonzalezV <45631973+VictorMGonzalezV@users.noreply.github.com>
2025-01-18 21:56:48 +0100

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(opt=> 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. Also called middleware
+if (app.Environment.IsDevelopment())
+{
+   
+    app.MapOpenApi();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//This using statement means the variable will be ddisposed of as soon as the code executes, instead of waiting for the garbage collector
+using var scope=app.Services.CreateScope();
+var services=scope.ServiceProvider;
+try
+{
+    //This is equivalent to running the ef migrate command via CLI
+    var context=services.GetRequiredService<DataContext>();
+    //Since SeedData is asynchronous, Migrate must be called using await+Async version
+    await context.Database.MigrateAsync();
+    //SeedData is an asynchronous Task, so it needs await before calling it
+    await Seed.SeedData(context);
+
+}catch(Exception ex)
+{
+    var logger=services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"Migration is no");
+}
+
+app.Run();
---
+++ Domain/Activity.cs
@@ -0,0 +1,20 @@
+namespace Domain
+{
+    public class Activity
+    {
+        //This property names are required by EF
+        public Guid Id { get; set; }
+        //Properties must be public and contain a getter and a setter for EF to work
+        public string Title { get; set; }
+
+        public DateTime Date{ get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string City { get; set; }
+
+        public string Venue { get; set; }
+    }
+}
